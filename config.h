431bdcd103ae1b9a31e4a43e51e238b52ccd5e92
Emilio <>
2022-09-25 12:01:48 +0200

    Bar padding

--- config.h
+++ config.h
@@ -6,6 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 12;       /* gap pixel between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 8;       /* vertical padding of bar */
+static const int sidepad            = 9;       /* horizontal padding of bar */
 static const char *fonts[]          = { "monospace:size=15" };
 static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
