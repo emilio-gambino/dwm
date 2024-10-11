0e7b51bd704db3560d2515b998b33041360ac6e1
Emilien Guandalino <emilien.guandalino@huawei.com>
2024-10-11 09:58:17 +0200

    Patch: Draw status bar on all monitors

--- dwm.c
+++ dwm.c
@@ -711,7 +711,7 @@ drawbar(Monitor *m)
 		return;
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
+	if (m == selmon || 1) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
 		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
 		drw_text(drw, m->ww - tw - 2 * sp, 0, tw, bh, 0, stext, 0);
@@ -2001,7 +2001,10 @@ updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
 		strcpy(stext, "dwm-"VERSION);
-	drawbar(selmon);
+
+	Monitor* m;
+	for(m = mons; m; m = m->next)
+		drawbar(m);
 }
 
 void
